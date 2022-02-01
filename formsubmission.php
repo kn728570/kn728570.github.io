e3bc02fc880c7ee58ba202f88a58c1e242c23495
kn728570 <98051956+kn728570@users.noreply.github.com>
2022-02-01 15:39:12 -0400

    Create formsubmission.php

---
+++ formsubmission.php
@@ -0,0 +1,19 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "contact@mrkenyannagy.com";
+    $from = $_POST['email']; // 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", I will contact you as soon as possible.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
